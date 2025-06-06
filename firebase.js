d21fa9b943c105007ca5589e7c3683f99a2ba554
Biti942 <biti.hassan@icloud.com>
2025-06-06 21:45:13 +0200

    Mise à jour du code AutoRent

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// js/firebase.js
+
+// Configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDcSbsP-ylos5DC-oHUuyB-PFG6rnNhvJk",
+  authDomain: "autorent-7b992.firebaseapp.com",
+  projectId: "autorent-7b992",
+  storageBucket: "autorent-7b992.appspot.com",
+  messagingSenderId: "824292845635",
+  appId: "1:824292845635:web:2eb167bad2c9ac4666ccf6",
+  measurementId: "G-DC3BM6BZVG"
+};
+
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Services utiles (tu pourras les utiliser partout)
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
