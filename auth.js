2d216559cad14e11123607687c298df732adb96c
Biti942 <biti.hassan@icloud.com>
2025-06-11 22:33:35 +0200

    ajout acc demo1

---
+++ auth.js
@@ -0,0 +1,22 @@
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    const uid = user.uid;
+
+    // UID admin
+    if (uid === "Ee4CN2XR0lZSLsFnSnipKo177oG2") {
+      window.location.href = "admin-dashboard.html";
+
+    // UID du compte démo
+    } else if (uid === "Ee4CN2XR0lZSLsFnSnipKo177oG2") {
+      window.location.href = "dashboard.html"; // accès direct
+
+    } else {
+      // Redirige les autres vers la page des packs
+      window.location.href = "choix-pack.html";
+    }
+
+  } else {
+    console.log("Utilisateur non connecté.");
+  }
+});
+
